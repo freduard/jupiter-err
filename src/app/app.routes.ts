3e2ae146c4479ea7a23ac4f440450fe46beebfda
Fredy Ait <aitfredy@gmail.com>
2025-07-06 21:34:06 +0300

    first working prot

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Video } from './video/video';
+import { Audio } from './audio/audio';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", component:Video},
+    {path: "audio", component:Audio}
+];
---
+++ src/app/video/video.ts
@@ -0,0 +1,27 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-video',
+  imports: [HttpClientModule],
+  templateUrl: './video.html',
+  styleUrl: './video.css'
+})
+export class Video implements OnInit {
+  httpClient = inject(HttpClient)
+  data: any[] = []
+
+  ngOnInit(): void {
+    this.fetchData()
+  }
+
+  fetchData() {
+    this.httpClient
+      .get('https://services.err.ee/api/v2/category/getByUrl?url=video&domain=jupiter.err.ee')
+      .subscribe((data: any) => {
+        console.log(data.data.category.frontPage)
+        this.data = data.data.category.frontPage
+        this.data.shift()
+      })
+  }
+}
